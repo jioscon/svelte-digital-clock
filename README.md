86661013d0d173acd479df04e4d1d108b4450e8b
Jio Scon <jioscon@gmail.com>
2022-02-11 10:33:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte Digital Clock
+
+REPL : <https://svelte.dev/repl/dd3449bddaed41b6808372cc05804690>
---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { onDestroy } from 'svelte';
+
+  let now = new Date();
+  $: hours = now.getHours();
+  $: minutes = now.getMinutes();
+  $: seconds = now.getSeconds();
+
+  const interval = setInterval( () => now = new Date(), 1000 );
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+</script>
+
+<span>{hours < 12 ? 'AM' : 'PM'} {hours} : </span>
+<span>{minutes < 10 ? 0 : ''}{minutes} : </span>
+<span>{seconds < 10 ? 0 : ''}{seconds}</span>
